24b7e12e719c70b68866495c40cd389854883b7f
Maksim Kudriavtsev <horokko@mail.ru>
2025-05-17 14:21:22 +0200

    fix: address lint errors

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import { Example } from './nodes/Example/Example.node';
+import { GitlabExtended } from './nodes/GitlabExtended/GitlabExtended.node';
 
-export const nodes = [Example];
+export const nodes = [Example, GitlabExtended];
