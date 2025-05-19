de2e19ca3a8073507d7283db6df3e095286ffd4e
Maksim Kudriavtsev <horokko@mail.ru>
2025-05-19 22:09:11 +0200

    refactor tests: extract createContext helper

---
+++ tests/helpers/createContext.js
@@ -0,0 +1,30 @@
+export default function createContext(params) {
+  const calls = {};
+  return {
+    calls,
+    getInputData() {
+      return [{ json: {} }];
+    },
+    getNodeParameter(name) {
+      return params[name];
+    },
+    async getCredentials() {
+      return { server: 'https://gitlab.example.com', accessToken: 't', projectId: 1 };
+    },
+    helpers: {
+      async requestWithAuthentication(name, options) {
+        calls.options = options;
+        return {};
+      },
+      constructExecutionMetaData(data) {
+        return data;
+      },
+      returnJsonArray(data) {
+        return [{ json: data }];
+      },
+    },
+    getNode() {
+      return {};
+    },
+  };
+}
